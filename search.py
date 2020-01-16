41b9f4740450a10fb482bc95e9f2f7811708213f
laptop_mi_ubuntu <woodywff@aliyun.com>
2020-01-16 23:01:26 +0800

    hello rep

---
+++ search.py
@@ -0,0 +1,18 @@
+import pdb
+
+
+class SearchingProcess():
+    '''
+    Main class for searching
+    '''
+    def __init__(self):
+        pass
+    
+    def search(self):
+        pass
+    
+    def train(self):
+        pass
+    
+    def infer(self):
+        pass
