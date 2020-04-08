ad1c6f5b8711b11e3945ba2c1425ae2d13ad18fc
laptop_mi_ubuntu <woodywff@aliyun.com>
2020-04-08 23:09:05 +0800

    plot

--- helper.py
+++ helper.py
@@ -25,10 +25,64 @@ def dim_assert(t_list):
 def print_red(something):
     print("\033[1;31m{}\033[0m".format(something))
     
+    
+def plot_cell(filename, n_nodes=3, dc=True, fmt='png'):
+    '''
+    filename: save path
+    n_nodes: number of nodes
+    dc: if True, for downward cell, otherwise for upward cell
+    '''
+    g = Digraph(format=fmt,
+                graph_attr = dict(dpi='800'),
+                edge_attr = dict(fontsize='20',penwidth='1.5'),
+                node_attr = dict(style='filled', shape='rect', align='center',
+                                 fontsize='20', height='0.1', width='0.1',
+                                 penwidth='2'),
+                engine='dot')
+    g.attr(rankdir='TB' if dc else 'BT')
+    g.node('x0', label='X0', fillcolor='white',shape='plaintext')
+    g.node('x1', label='X1', fillcolor='white', shape='plaintext')
+    g.node('p0', label='pre0(s=2)' if dc else 'pre0(s=1)', fillcolor='ghostwhite')
+    g.node('p1', label='pre1(s=1)', fillcolor='ghostwhite')
+    g.edge('x0', 'p0')
+    g.edge('x1', 'p1')
+    
+    xs = ['p0','p1']
+    for n_ops in range(2,2+n_nodes):
+        node_i = n_ops-2
+        with g.subgraph(name='cluster_{}'.format(node_i)) as sg:
+            sg.attr(style='dashed', color='red', label='node {}'.format(node_i), 
+                    fontsize='20', penwidth='1.8', fontcolor='red')
+            name_add = 'add'+str(node_i)
+            sg.node(name=name_add, label='+', fillcolor='lightskyblue2')
+            with sg.subgraph() as ssg:
+                ssg.attr(rank='same')
+                for op_i in range(n_ops):
+                    name_op = 'n{}_{}'.format(node_i, op_i)
+                    if dc and op_i < 2:
+                        label = 'D'
+                    elif not dc and op_i == 1:
+                        label = 'U'
+                    else:
+                        label = 'N'
+                    ssg.node(name=name_op, label=label, fillcolor='ghostwhite')
+                    g.edge(xs[op_i], name_op)
+                    g.edge(name_op, name_add)
+        xs.append(name_add)
+        
+    g.node('concat', label='C', fillcolor='wheat')
+    for name in xs[-3:]:
+        g.edge(name, 'concat')
+    g.node('y', label='Y', fillcolor='white', shape='plaintext')
+    g.edge('concat','y')
+    g.render(filename)
 
-def visualize(genotype, filename, dc=True, fmt='png'):
+def plot_searched_cell(genotype, filename, dc=True, fmt='png'):
     '''
     Draw searched downward and upward cells.
+    filename: save path
+    genotype: Genotype.down or Genotype.up, Genotype is defined in genotype.py
+    dc: if True, for downward cell, otherwise for upward cell
     '''
 #     pdb.set_trace()
     g = Digraph(format=fmt,
